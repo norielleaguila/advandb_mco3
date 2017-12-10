43e6c382277f187cba41247ac31210ec217f7157
Norielle Aguila <norielle_aguila@dlsu.edu.ph>
2017-12-10 21:29:32 +0800

    updated pulling

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,16 @@
-const db = firebase.database().ref().child('regions');
+const db = firebase.database().ref();
 
 db.on('value', snap => {
     var n = 0;
     var x = $("#window-content");
     
-    snap.forEach(function(data){
-        
-        x.append("<b>" + data.key + "</b>");
-        
-        x.append(
+    // create table
+    x.append(
             "<table>" +
                 "<thead id='table_head" + n + "'>" +
                     "<tr>" +
-                        "<td><b>Country Code</b></td>" +
+                        "<td><b>Country Name</b></td>" +
+                        "<td><b>Region</b></td>" +
                         "<td><b>Income</b></td>" +
                     "</tr>" +
                 "</thead>" +
@@ -21,18 +19,47 @@ db.on('value', snap => {
              "</table>"
             );
     
+//    // iterate through children
+//    snap.forEach(function(data){
+//        var b = $('#table_body' + n);
+//        
+//        var country = data.val();
+//        
+//        b.append("<tr><td>" + country.CountryName +  "</td><td>" + country.Region + "</td>" + "</td><td>" + country.Income + "</td><tr>");
+//        
+//        // iterate through values of each series
+//        data.forEach(function(sdata){
+//            
+//
+//        });
+//        
+//    });
+//        
+//    n++;
+    
+    // iterate through nodes
+    snap.forEach(function(data){
+        
         var b = $('#table_body' + n);
-
+        
+        // iterate through values of each node
         data.forEach(function(cdata){
+            
             var country = cdata.val();
-
-            b.append("<tr><td>" + cdata.key + "</td><td>" + country.income + "</td><tr>");
-
+        
+            b.append("<tr><td>" + country.CountryName +  "</td><td>" + country.Region + "</td>" + "</td><td>" + country.Income + "</td><tr>");
+            
         });
         
-        n++;
-        
     });
+        
+    n++;
 });
+
+
+
+
+
+
 
 
